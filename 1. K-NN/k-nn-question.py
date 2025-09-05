f3c841a79b1561ffd7498176c36c24bdfe334a13
Fun <hfrommane@qq.com>
2025-09-05 11:04:39 +0800

    Add files via upload

--- 1. K-NN/k-nn-question.py
+++ 1. K-NN/k-nn-question.py
@@ -1,2 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# Create sample dataset
+data = {
+    'Movie title': [
+        'California Man',
+        'He\'s Not Really into Dudes',
+        'Beautiful Woman',
+        'Kevin Longblade',
+        'Robo Slayer 3000',
+        'Amped II',
+        '?'
+    ],
+    '# of kicks': [3, 2, 1, 101, 99, 98, 18],
+    '# of hugs': [104, 100, 81, 10, 5, 2, 90],
+    'Movie Type': ['Romance', 'Romance', 'Romance', 'Action', 'Action', 'Action', 'Unknown']
+}
